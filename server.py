d5bfc9e51cb4ee88c50052426b73e900c87e7f33
Kiera Cox <kiera.cox18@gmail.com>
2023-04-19 15:41:09 -0700

    app routes

--- server.py
+++ server.py
@@ -9,13 +9,13 @@ app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
-def homepage():
+def show_homepage():
     """View homepage."""
     return render_template('homepage.html')
 
-@app.route('/users', methods=['POST'])
-def register_recruiter():
-    """Create a new recruiter account."""
+@app.route('/register', methods=['POST'])
+def register_user():
+    """Create a new user account."""
     email = request.form.get("email")
     password = request.form.get("password")
     user_type = request.form.get("user_type")
@@ -31,20 +31,19 @@ def register_recruiter():
     elif user_type == "recruiter":
         recruiter = crud.create_recruiter(email, password)
         db.session.add(recruiter)
-        db.session.add(recruiter)
         db.session.commit()
-        flash("Account created! Please log in.")
+        session["user_email"] = user.email
+        session["user_type"] = user_type
+        return redirect("/user_dashboard")
     
     else: 
         job_seeker = crud.create_job_seeker( email, password)
         db.session.add(job_seeker)
-        db.session.add(job_seeker)
         db.session.commit()
-        flash("Account created! Please log in.")
+        session["user_email"] = user.email
+        session["user_type"] = user_type
+        return redirect("/user_dashboard")
     
-    return redirect("/") #change to redirect to user dashboard
-
-
 @app.route("/login", methods=["POST"])
 def log_in_user():
     """Log in a user."""
@@ -63,21 +62,66 @@ def log_in_user():
         session["user_email"] = user.email
         session["user_type"] = user_type
         flash(f"Welcome back, {user.fname}!")
-    #possibly need to add /<id>
-    return redirect("/")
+    return redirect("/user_dashboard")
 
 @app.route("/user_dashboard")
 def show_dashboard():
-    """Display a user dashboard"""
+    """Display a user dashboard."""
     user_email = session.get("user_email")
     user_type = session.get("user_type")
 
     if user_type == "job_seeker":
         user = crud.get_js_by_email(user_email)
-        return render_template("jobseeker_dashboard.html")
+        return render_template("js_dashboard.html", user=user)
     else:
         user = crud.get_recruiter_by_email(user_email)
         return render_template("recruiter_dashboard.html", user=user)
+    
+@app.route("/user_profile")
+def show_profile():
+    """Display a user's profile page."""
+    user_type = session.get("user_type")
+    user_email = session.get("user_email")
+
+    if user_type == "job_seeker":
+        user = crud.get_js_by_email(user_email)
+        return render_template("js_profile.html", user=user)
+    else:
+        user = crud.get_recruiter_by_email(user_email)
+        return render_template("recruiter_profile.html", user=user)
+
+@app.route("/new_search")
+def show_search():
+    """Display the search page."""
+    user_type = session.get("user_type")
+    user_email = session.get("user_email")
+
+    if user_type == "job_seeker":
+        user = crud.get_js_by_email(user_email)
+        return render_template("js_search.html", user=user)
+    else:
+        user = crud.get_recruiter_by_email(user_email)
+        return render_template("recruiter_search.html", user=user)
+    
+@app.route("/search", methods=['POST'])
+def run_search():
+    """Run a new user search."""
+    #TODO: finish this function to take in form inputs
+    return redirect("/search_results")
+    
+@app.route("/search_results")
+def show_results():
+    """Display a user's search results."""
+    user_type = session.get("user_type")
+    user_email = session.get("user_email")
+
+    if user_type == "job_seeker":
+        user = crud.get_js_by_email(user_email)
+        return render_template("js_search_results.html", user=user)
+    else:
+        user = crud.get_recruiter_by_email(user_email)
+        return render_template("recruiter_search_results.html", user=user)
+
     
 
 
